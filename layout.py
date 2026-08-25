1566667d1925c6cd09b0a8fa046384bfefd6c162
chandrakanth pokuru <chandrakanth_pokuru2@comcast.com>
2026-08-25 17:23:28 -0400

    phase2 enh33

--- layout.py
+++ layout.py
@@ -731,6 +731,9 @@ def render_page(active_id: str, head_extra: str, body_html: str, script: str = "
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="0">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;600;700&family=Inter:wght@400;500;600;700;800&family=JetBrains+Mono:wght@400;500;600&display=swap" rel="stylesheet">
